2475908eb40a1e1456c73fb30c2403a3626f4832
tanyaka <litium@yandex.ru>
2023-10-24 04:01:17 +0300

    validation

--- models/movie.js
+++ models/movie.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 // Регулярное выражение для проверки ссылок
 const linkValid =
-  /^(https?:\/\/)(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[\w\-./#?&]*)*$/;
+  // eslint-disable-next-line no-useless-escape
+  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/i;
 
 const movieSchema = new mongoose.Schema({
   country: {
